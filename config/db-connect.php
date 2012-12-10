6f3709b1a7b7e30da11caf8468ee5f89db4d2b7b
Lovell Felix <lovell.felix@gmail.com>
2012-12-09 23:35:43 -0600

    Added screenshots

---
+++ config/db-connect.php
@@ -0,0 +1,12 @@
+<?php
+
+////////////////////
+// This file contains the database access information. 
+// This file is needed to establish a connection to MySQL
+
+$host = "localhost"; // localhost normally works, if localhost doesn't exist contact your web host
+$dbName = "ems"; // Database name
+$dbUser = "ems"; // Username
+$dbPass = "ems"; // Password
+
+?>
